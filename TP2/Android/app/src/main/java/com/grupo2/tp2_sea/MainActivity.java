f583d344570a9ac84ff8106f220b94b04764a362
SebastianIRodriguez <seba.rodriguez.994@gmail.com>
2024-06-08 21:20:35 -0300

    Agrego app vacia de android, con la biblioteca de mqtt ya importada

---
+++ TP2/Android/app/src/main/java/com/grupo2/tp2_sea/MainActivity.java
@@ -0,0 +1,31 @@
+package com.grupo2.tp2_sea;
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.OnApplyWindowInsetsListener;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), new OnApplyWindowInsetsListener() {
+            @NonNull
+            @Override
+            public WindowInsetsCompat onApplyWindowInsets(@NonNull View v, @NonNull WindowInsetsCompat insets) {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            }
+        });
+    }
+}
---
+++ TP2/Android/app/src/main/java/com/grupo2/tp2_sea/MqttConnection.java
@@ -0,0 +1,6 @@
+package com.grupo2.tp2_sea;
+
+public class MqttConnection {
+    //Codigo de ejemplo:
+    //https://github.com/eclipse/paho.mqtt.java/tree/master/org.eclipse.paho.sample.mqttclient/src/main/java/org/eclipse/paho/sample/mqttclient/mqttv5
+}
